698609741454c3ff5e0bb1c82ed271a0eb264bc8
sajjadjawaid <sajjadjawaid@gmail.com>
2022-03-10 17:59:56 +0500

    commit

--- Java/forecast.js
+++ Java/forecast.js
@@ -1,7 +1,7 @@
 
 const apikey="8dRIIA0juR0Y2TAJXmVShIUZDP0xMnEW"
 const getCityInfo= async (city)=>{
-    const baseurl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const baseurl = 'https://dataservice.accuweather.com/locations/v1/cities/search';
     const query = `?apikey=${apikey}&q=${city}`;
     const cityInfo = await fetch(baseurl+query);
     const data = await cityInfo.json();
@@ -13,7 +13,7 @@ const getCityInfo= async (city)=>{
 getCityInfo('Lahore')
 
 const getWeather = async (id)=> {
-    const baseurl = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const baseurl = 'https://dataservice.accuweather.com/currentconditions/v1/'
     const query =`${id}?apikey=${apikey}`
     const res = await fetch(baseurl+query)
     const data= await res.json();
